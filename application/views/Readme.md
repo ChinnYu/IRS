46920afad52009c644d60b5c2ae7e00f9ff2fbd2
TLOGBen <powdersave@gmail.com>
2017-12-25 17:28:07 +0800

    12/25 update : View結構文件 ========================= 1.更新資料夾內readme

---
+++ application/views/Readme.md
@@ -0,0 +1,11 @@
+View資料夾內程式對應網頁
+=====================
+* 首頁 index
+    * 登錄頁面 login
+    * 登錄錯誤頁面 login_error
+    * 學生首頁 StudentHomePage
+        
+    * 教師首頁 TeacherHomePage
+        * 新增題目頁面 TeacherHome_EditQuestion
+        * 編輯題目頁面 TeacherHome_NewQuestion
+        * 新增考卷頁面 TeacherHome_NewQuiz
--- recourse/js/PinAndQr.js
+++ recourse/js/PinAndQr.js
@@ -6,7 +6,7 @@ $.ajax({
         var Pin = res;
         if (res == "") {
             document.getElementById("Pin").innerHTML = Pin;
-            document.getElementById("QRShow").innerHTML = "<img src='https://chart.googleapis.com/chart?chs=382x300&cht=qr&chl=" + encodeURIComponent(pin) + "'/>";
+            document.getElementById("QRShow").innerHTML = "<img src='https://chart.googleapis.com/chart?chs=382x300&cht=qr&chl=" + encodeURIComponent(Pin) + "'/>";
         }
     }
 });
