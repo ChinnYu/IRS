102be798a72dd8215e31b6ab7f6f7572bdc024b2
TLOGBen <powdersave@gmail.com>
2018-01-02 17:16:43 +0800

    update 0102: Ckeditor update =============================== 1. update ckeditor lib in recourse/js 2. update ckeditor/ckfinder library in application/library 3. update Ckeditor usage example in application/view/Example

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ IRS for Querisma
 * Application 資料夾存放前後控制端
 * Recourse 存放所使用的資源
 
-版本號：6 <br>
+版本號：6.1 <br>
 
-新增變動見注意
+新增變動: Ckeditor
+1.  recource/js 內部新增Ckeditor.js <br>
+    用於進行Ckeditor的動作
+2.  application/library 內部新增“ckeditor”、“ckfinder” <br>兩個函式庫，使用時請呼叫內部的Ckeditor.js
+3.  application/view 新增了Example/index.html<br>
+    裡面有使用範例
---
+++ recourse/js/Ckeditor.js
@@ -0,0 +1,34 @@
+/**
+ * @函式名稱｜    outputEditorContent
+ * @功能    |    Editor的內容存成Data輸出
+ * @觸發方式 |    事件呼叫函式 觸發，將Editor內部內容抓出來
+ * @輸入變數 |    editor_input : (String) 目標編輯器的id
+ * @函式輸出 ｜   editor_content : (String) 目標編輯器的內文,Html格式
+ */
+function outputEditorContent(editor_input) {
+    //Output Data from id = 'ckeditor' editor place(Textarea/Div);
+    return CKEDITOR.instances[editor_input].getData();
+}
+
+/**
+ * @函式名稱｜    inputEditorContent
+ * @功能    |    將Data的東西放上目標位置
+ * @觸發方式 |    事件呼叫函式 觸發，將Data內部內容部署到目標位置
+ * @輸入變數 |    target_id : (String) 要部署的id | editor_content : (String) 目標資料
+ * @函式輸出 ｜    無
+ */
+function inputEditorContent(target_id, editor_content) {
+    document.getElementById(target_id).innerHTML = editor_content;//insert What you write in editor
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, this.formula]); //Rewrite Tex code in Data
+}
+
+/**
+ * @函式名稱｜    directTransfer2Input
+ * @功能    |    直接將editor內容轉入目標位置
+ * @觸發方式 |    事件呼叫函式 觸發，將editor內容部署到目標位置
+ * @輸入變數 |    target_id : (String) 要部署的id | editor_input : (String) 編譯器id
+ * @函式輸出 ｜    無
+ */
+function directTransfer2Input(editor_input, target_id) {
+    inputEditorContent(target_id, outputEditorContent(editor_input));
+}
