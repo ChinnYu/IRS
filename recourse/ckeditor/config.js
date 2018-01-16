3e569a7e198b6357f3eef791c464c65a15432c5b
TLOGBen <powdersave@gmail.com>
2018-01-16 12:26:03 +0800

    0116 update : Ckeditor 功能更新 && CKFinder簡易上傳 ======================== 1. 更新Ckeditor編輯器對編輯器輸出 2. 更新Ckeditor自動轉編輯器功能 3. 更新Ckeditor和Ckfinder的Lib位置 4. 新增圖片上傳功能（第一版） 5. 使用時 請去/recourse/ckfinder/config.php 第63行之下的“Backends”更改目錄與結構 6. 上傳資料夾放在/IRS/ 的/User 請自行調整位置 並且給他Chmod 777

--- recourse/ckeditor/config.js
+++ recourse/ckeditor/config.js
@@ -10,22 +10,21 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
+		//{ name: 'clipboard',   groups: [ 'undo' ] },
+		//{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		//{ name: 'links' },
 		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		//{ name: 'forms' },
+		//{ name: 'tools' },
+		//{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		//{ name: 'others' },
+		{ name: 'basicstyles', groups: [ 'basicstyles' ] }
+		//{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		//{ name: 'styles' },
+		//{ name: 'colors' }
+		//{ name: 'about' }
 	];
-    config.fullPage = true;
+    config.fullPage = false;
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
