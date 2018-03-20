c7e32bf25097c0b22fa39f6fde0337659d79aaf5
ChinnYu <32726550+ChinnYu@users.noreply.github.com>
2018-03-20 23:46:08 +0800

    修改前端網頁&增加cookie 可記住使用者
    修改前端網頁(index.html大改)&增加cookie 可記住使用者

--- application/controllers/Studenthome.php
+++ application/controllers/Studenthome.php
@@ -7,5 +7,54 @@
 			$res=$this->userinfo;
 			echo json_encode($res['user_Name']);
 		}
-	
+		
+		public function set_cookie(){
+			if(isset($_SESSION['remember'])){
+				if($_SESSION['remember']==1){
+					$times=date("Y-m-d");
+					$coo_temp=$_SESSION['user']['user_Id']."-".$times;
+					$data=$this->encrypt_cookies($coo_temp);
+					unset($_SESSION['remember']);
+					echo json_encode($data);
+				}else{
+					unset($_SESSION['remember']);
+					echo json_encode(FALSE);
+				}
+			}else{
+				echo json_encode(FALSE);
+			}
+		}
+		
+		public function encrypt_cookies($data){
+			$this->load->library('encryption');
+			$this->encryption->initialize(
+				array(
+					'cipher' => 'aes-128',
+					'mode' => 'cbc',
+					'key' => '8888',
+				)
+			);
+			$ciphertext = $this->encryption->encrypt($data);
+			$data=base64_encode($ciphertext);
+			return $data;
+		}
+		
+		public function check_cookies(){
+			if(isset($_SESSION['update_coo'])){
+				if($_SESSION['update_coo']==1){
+					$times=date("Y-m-d");
+					$coo_temp=$_SESSION['user']['user_Id']."-".$times;
+					$data=$this->encrypt_cookies($coo_temp);
+					unset($_SESSION['update_coo']);
+					echo json_encode($data);
+				}else{
+					unset($_SESSION['update_coo']);
+					echo json_encode(FALSE);
+				}
+			}else{
+				echo json_encode(FALSE);
+			}
+			
+		}
+		
 	}
