07c21bac52c255567f27b2c2515184490bd2f17a
BenTisa <powdersave@gmail.com>
2018-04-13 09:26:48 +0800

    0412 Ckeditor.js update ======================= * open file browser * new Readonly mode function * change some function content

--- recourse/js/Ckeditor.js
+++ recourse/js/Ckeditor.js
@@ -30,7 +30,7 @@ const editorConfig = {
  */
 function autoTransferInlineEditor(editor_id) {
 	CKEDITOR.inline(editor_id, editorConfig);
-	switchReadonlyMode(editor_output,'off');
+	switchReadonlyMode(editor_output, 'off');
 }
 
 /**
@@ -74,7 +74,7 @@ function outputEditorContent(editor_input) {
 function inputOriginalData(editor_output, data_content) {
 	CKEDITOR.instances[editor_output].setData(data_content);
 	CKEDITOR.instances[editor_output].editables = true;
-	switchReadonlyMode(editor_output,'off');
+	switchReadonlyMode(editor_output, 'off');
 }
 
 /**
@@ -83,7 +83,7 @@ function inputOriginalData(editor_output, data_content) {
  * @觸發方式     事件呼叫函式 觸發，將Data內部內容部署到目標位置
  * @輸入變數     target_id : (String) 要部署的id | editor_content : (String) 目標資料
  * @函式輸出     無
- * @備註			使用此function並不會使目標區域變成可編輯;不會使目標區域便編輯器
+ * @備註            使用此function並不會使目標區域變成可編輯;不會使目標區域便編輯器
  */
 function inputEditorContent(target_id, editor_content) {
 	document.getElementById(target_id).innerHTML = editor_content;//insert What you write in editor
@@ -113,18 +113,18 @@ function directTransfer2Input2(editor_input, editor_output) {
 
 /**
  * @函式名稱 switchReadonlyMode
- * @param opinion 開關選擇(in/off)
+ * @param opinion 開關選擇(on/off)
  * @param target_id 輸出目標區塊的id
  * @用法 將想修改的區域 id 輸入,並輸入on/off來開關是否ReadOnly 該區域是否能夠編輯
  */
 function switchReadonlyMode(opinion, target_id) {
 	let introduction = document.getElementById(target_id);
 	if (opinion.trim() === "on") {
-		introduction.setAttribute('contenteditable', false);
+		introduction.setAttribute("contenteditable", false);
 	}
 	else {
 		if (!("off" === opinion.trim())) {
-			introduction.setAttribute('contenteditable', true);
+			introduction.setAttribute("contenteditable", true);
 		}
 		else {
 			console.error("ReadOnly on/off input failure");
@@ -139,7 +139,7 @@ function switchReadonlyMode(opinion, target_id) {
  * @備註 此方法轉換後 目標區域一定無法編輯
  */
 function autoTurnMath(target_id) {
-	switchReadonlyMode(target_id,'off');
+	switchReadonlyMode('off', target_id);
 	changeMathIntoTex();
-	switchReadonlyMode(target_id,'on');
+	switchReadonlyMode('on', target_id);
 }
