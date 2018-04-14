a5d77c88bf083577893272d526fae1cfe9e351a5
BenTisa <powdersave@gmail.com>
2018-04-14 17:53:38 +0800

    0414 url update ======================= * fix bug

--- recourse/js/PinAndQr.js
+++ recourse/js/PinAndQr.js
@@ -1,13 +1,11 @@
 $.ajax({
-    url: "openclass/getPin",
-    type: "POST",
-	dataType : "text",
-    success : function (res) {
-        var Pin = res;
-		if (res=="") {
-			document.getElementById("Pin").innerHTML = Pin;
-			document.getElementById("QRShow").innerHTML = "<img src='https://chart.googleapis.com/chart?chs=382x300&cht=qr&chl=" + encodeURIComponent(pin) + "'/>";
-		}
+	url: "openclass/getPin",
+	type: "POST",
+	dataType: "text",
+	success: function (res) {
+		let Pin = res;
+		let Pin_url = "https://querisma.ccu.edu.tw/~tlogben0709/www/www/scanQR?Pin=" + encodeURIComponent(Pin);
+		document.getElementById("Pin").innerHTML = Pin;
+		document.getElementById("QRShow").innerHTML = "<img src='https://chart.googleapis.com/chart?chs=546x547&cht=qr&chl=" +  encodeURIComponent(Pin_url) + "'data-action=\"zoom\"/>";
 	}
 });
-
