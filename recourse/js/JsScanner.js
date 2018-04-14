7d7757c8f255d19294402ce857f75b5e5432ce6e
BenTisa <powdersave@gmail.com>
2018-04-14 21:00:05 +0800

    04.14 Url update ======================= * Update Url things * auto submit

--- recourse/js/JsScanner.js
+++ recourse/js/JsScanner.js
@@ -1,9 +1,9 @@
 function onQRCodeScanned(scannedText) {
-    var scannedTextMemo = document.getElementById("scannedTextMemo");
+    let scannedTextMemo = document.getElementById("scannedTextMemo");
     if (scannedTextMemo) {
-
-		let pin = scannedText.slice(-4);
-        scannedTextMemo.value = pin;
+    	let url = new  URL(scannedText);
+		scannedTextMemo.value = url.searchParams.get("Pin");
+        document.getElementById("form_submit").submit();
     }
 }
 
@@ -11,10 +11,10 @@ function onQRCodeScanned(scannedText) {
 function JsQRScannerReady() {
     //create a new scanner passing to it a callback function that will be invoked when
     //the scanner succesfully scan a QR code
-    var jbScanner = new JsQRScanner(onQRCodeScanned);
+    let jbScanner = new JsQRScanner(onQRCodeScanned);
     //reduce the size of analyzed images to increase performance on mobile devices
     jbScanner.setSnapImageMaxSize(300);
-    var scannerParentElement = document.getElementById("scanner");
+    let scannerParentElement = document.getElementById("scanner");
     if (scannerParentElement) {
         //append the jbScanner to an existing DOM element
         jbScanner.appendTo(scannerParentElement);
