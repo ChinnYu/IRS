ad8169be3463ea184fc607bfa218fe5c09c81a00
kuangching <30891693+kuangching@users.noreply.github.com>
2018-04-23 02:07:35 +0800

    endclass學生端更新

--- application/controllers/Longpolling.php
+++ application/controllers/Longpolling.php
@@ -24,13 +24,11 @@
 			//header("Location: ../QuizPage");			
 		}
 		public function polling2(){
-		$this->load->model('this_class_model','this_class');
-		$class_Id=array(
-				'class_Id'=>$_SESSION['classandquiz']['class_Id']
-			);
-			$endClass=$this->this_class->findEndclass($class_Id); 
-			$row = $endClass[0]['end_Class'];
-			echo $row;
+			$this->load->library('MP_Cache');
+			$cdata_name=$_SESSION['classandquiz']['class_Id'].'_endclass';
+			$cdata_endclass = $this->mp_cache->set_name($cdata_name)->get();
+
+			echo $cdata_endclass['endclass'];
 		}
 	}
 	
